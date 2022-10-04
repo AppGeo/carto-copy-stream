5b01101c527c57bd53c8bab6d063aedbbb5b8d0d
Calvin W. Metcalf <cmetcalf@appgeo.com>
2022-10-04 10:30:07 -0400

    update deps and test

--- test.js
+++ test.js
@@ -3,11 +3,11 @@ let config = require('./config.json');
 let cartoStream = require('./');
 
 let stream = cartoStream(config.user, config.key, 'copy_test', ['the_geom', 'foo', 'bar'], (err, resp) => {
-  console.log('done', err, resp.total_rows);
+  console.log('done', err, resp?.total_rows);
 })
 // stream.write(data[2], e => console.log('item done', e));
 
-for (let item of data) {
+for (let item of data.features) {
   // console.log('item', item)
   stream.write(item, e => console.log('item done', e));
 }
